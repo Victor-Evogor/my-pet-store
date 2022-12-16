6390eb5ccd2ba18ea68cc974ce54040951b7b4e5
Victor-Evogor <victorevogor0001@gmail.com>
2022-12-16 20:15:57 +0100

    Fixed a bug

--- components/SlidingNavBar.tsx
+++ components/SlidingNavBar.tsx
@@ -12,14 +12,14 @@ const SlidingNavBar: FunctionComponent<SlidingNavBarProps> = ({open, children}) 
     const parentVariant:Variants = {
         open:{
             left: 0,
-            opacity: 1
+            opacity: 1,
         },
         closed: {
             left: "-60%",
-            opacity: 0
+            opacity: 0,
         }
     }
-    return ( <motion.div className="fixed w-3/5 h-full top-0 left-0 bg-slate-100 bg-opacity-70 z-40 px-4 pt-20 overflow-y-auto" animate={open?"open":"closed"} variants={parentVariant}>
+    return ( <motion.div className="fixed w-3/5 h-full top-0 left-0 bg-slate-100 bg-opacity-70 z-40 px-4 pt-20 overflow-y-auto block sm:hidden" animate={open?"open":"closed"} variants={parentVariant}>
         {children}
     </motion.div> );
 }
